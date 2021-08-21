ec0d5e4a095f4ccd82d346c5567f55d9c0ca21ca
kamorulane <katlego.morulane@metropolitan.co.za>
2021-08-21 18:52:55 +0200

    I hope this works

--- jenkins/scripts/ami-web.sh
+++ jenkins/scripts/ami-web.sh
@@ -1,5 +1,5 @@
 cd terraform
-packer build -machine-readable packer-ami-web.json | sudo tee build.log
+ARTIFACT_WEB=`/usr/local/bin/packer build -machine-readable packer-ami-web.json  | awk -F, '$0 ~/artifact,0,id/ {print $6}'`
 AMI_ID_WEB=`echo $ARTIFACT_WEB | cut -d ':' -f2`
 echo 'variable "WEB_INSTANCE_AMI" { default = "'${AMI_ID_WEB}'" }' > amivar_web.tf
 aws s3 cp amivar_web.tf s3://metmomk-aws-jenkins-terraform/amivar_web.tf
